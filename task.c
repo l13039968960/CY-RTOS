48004d916812e8aacf7cfb532a0ba3189aaf5414
zw1194 <14207431+zw1194@user.noreply.gitee.com>
2026-02-22 20:50:56 +0800

    实现创建任务删除任务函数

---
+++ task.c
@@ -0,0 +1,78 @@
+/*************************************************************************
+ * @file     : task.c
+ * @brief    : 任务相关函数
+ * @author   : zw1194
+ * @date     : 2026-02-21
+ * @version  : V1.0
+ * @copyright:
+ * @note     :
+ *************************************************************************/
+
+#include "task.h"
+#include "heap.h"
+
+/**
+ * @brief  任务创建函数
+ * @param  taskhandle: 任务句柄
+ * @param  taskfuction: 任务函数
+ * @param  task_priority: 任务优先级
+ * @param  task_stack_size: 任务栈大小
+ * @return  pdTRUE: 创建成功
+ *          pdFALSE: 创建失败
+ * @note
+ */
+state_return TaskCreate(TaskHandle_t *taskhandle, TaskFunction taskfuction, uint8_t task_priority, uint8_t task_stack_size)
+{
+    state_return xreturn = pdTRUE;
+    memaddress stack_address, TCB;
+    stack_address = MemAllocate(task_stack_size);
+    if (stack_address != NULL)
+    {
+        TCB = MemAllocate(sizeof(TaskHandle));
+        if (TCB != NULL)
+        {
+            *taskhandle = (TaskHandle *)TCB;
+            (*taskhandle)->task_priority = task_priority;
+            (*taskhandle)->task_stack = stack_address;
+            (*taskhandle)->task_stack_size = task_stack_size;
+            (*taskhandle)->task_state = task_ready_state;
+            (*taskhandle)->taskfuction = taskfuction;
+        }
+        else
+        {
+            MemFree(stack_address);
+            xreturn = pdFALSE;
+        }
+    }
+    else
+    {
+        xreturn = pdFALSE;
+    }
+    return xreturn;
+}
+
+/**
+ * @brief  任务删除函数
+ * @param  taskhandle：将要删除的任务句柄
+ * @return  pdTRUE: 删除成功
+ *          pdFALSE: 删除失败
+ * @note
+ */
+state_return TaskDelete(TaskHandle_t *taskhandle)
+{
+    state_return xreturn = pdTRUE;
+    if (taskhandle != NULL)
+    {
+        state_return Free_state = MemFree((*taskhandle)->task_stack);
+        if (Free_state != pdTRUE)
+            xreturn = pdFALSE;
+        Free_state = MemFree(*taskhandle);
+        if (Free_state != pdTRUE)
+            xreturn = pdFALSE;
+    }
+    else
+    {
+        xreturn = pdFALSE;
+    }
+    return xreturn;
+}
--- task.h
+++ task.h
@@ -4,31 +4,33 @@
  * @author   : zw1194
  * @date     : 2026-02-21
  * @version  : V1.0
- * @copyright: 
- * @note     : 
+ * @copyright:
+ * @note     :
  *************************************************************************/
-#include "data.h"
+#ifndef __TASK_H__
+#define __TASK_H__
 
+#include "project_def.h"
+#include "os_config.h"
 
-typedef struct {
-    
-}TaskHandle;
+#define task_ready_state 1
+#define task_running_state 0
+#define task_blocked_state -1
 
+typedef struct
+{
+    TaskFunction taskfuction;
+    memaddress task_stack;
+    uint8_t task_state;
+    uint8_t task_priority;
+    uint8_t task_stack_size;
+} TaskHandle;
 
-/**
- * @brief  任务创建函数
- * @param  
- * @param  
- * @return 
- * @note   
- */
-TaskHandle TaskCreate(void);
+typedef TaskHandle* TaskHandle_t;
+/*任务创建函数*/
+state_return TaskCreate(TaskHandle_t *taskhandle, TaskFunction taskfuction, uint8_t task_priority, uint8_t task_stack_size);
 
-/**
- * @brief  任务删除函数
- * @param  taskhandle：删除的任务句柄
- * @return 
- * @note   
- */
-void TaskDelete(TaskHandle taskhandle);
+/*任务删除函数*/
+state_return TaskDelete(TaskHandle_t *taskhandle);
 
+#endif
